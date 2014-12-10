3f91a92173e9ef70fac81e7080ad12c29b739579
unknown <AWy@ImsUb01AWy.ims.ch>
2014-12-10 17:40:07 +0100

    Init

---
+++ target/generated-sources/annotations/org/books/persistence/entity/Login_.java
@@ -0,0 +1,13 @@
+package org.books.persistence.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-10T17:39:31")
+@StaticMetamodel(Login.class)
+public class Login_ { 
+
+    public static volatile SingularAttribute<Login, Long> id;
+
+}
